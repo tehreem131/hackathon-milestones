14793eac99dbc46ff8e9cf7b745fe3ee31587caa
tehreem131 <tehreemkhanzada23@gmail.com>
2024-09-17 09:50:12 -0700

    Add files via upload

--- script.ts
+++ script.ts
@@ -1,10 +1,36 @@
-const toggleButton = document.getElementById(`toggle-skills`)as HTMLButtonElement
-const skills = document.getElementById(`skills`) as HTMLElement
+const form = document.getElementById(`Rasume-form`) as HTMLElement;
 
-toggleButton.addEventListener(`click`, ()=> {
-    if(skills.style.display === `none`){
-        skills.style.display = `block`
-    } else {
-        skills.style.display = `none`
-    }
+form.addEventListener(`submit`, (event: Event) => {
+    event.preventDefault();
+
+    const name = (document.getElementById(`name`) as HTMLInputElement).value
+    const email = (document.getElementById(`email`) as HTMLInputElement).value
+    const phone = (document.getElementById(`phone`) as HTMLInputElement).value
+    const Education = (document.getElementById(`Education`) as HTMLInputElement).value
+    const Experince = (document.getElementById(`Experince`) as HTMLInputElement).value
+    const skills = (document.getElementById(`skills`) as HTMLInputElement).value
+
+    const resumeHTML = `
+            <h2><b>Resume</h2>
+            <h3>Personal Information</h3>
+            <p><b>Name:</b>${name}</p>
+            <p><b>email:</b>${email}</p>
+            <p><b>phone:</b>${phone}</p>
+
+            <h3>Education</h3>
+            <p>${Education}</p>
+
+            <h3>Experince</h3>
+            <p>${Experince}</p>
+
+            <h3>Skills</h3>
+            <p>${skills}</p>
+        `;
+        if (document.getElementById(`Rasume-display`) as HTMLDivElement){
+            (document.getElementById(`Rasume-display`) as HTMLDivElement).innerHTML = resumeHTML;
+        }else {
+            console.error(`The resume display element is missing.`);
+            
+        }
+
 });
